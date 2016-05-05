8ea485badb56f8718a09c209333d0d49c06ba106
Elechero <davidhervil@hotmail.com>
2016-05-05 18:37:30 -0430

    READYXON

--- Lexer/LexNeo.py
+++ Lexer/LexNeo.py
@@ -8,7 +8,7 @@
 # ------------------------------------------------------------
 from __future__ import print_function
 import ply.lex as lex
-COLUMNA = [1]
+COLUMNA = [0,1,0]
 # List of token names.   This is always required
 tokens = ('TkComa','TkPunto','TkDosPuntos','TkParAbre','TkParCierra',\
 'TkCorcheteAbre','TkCorcheteCierra','TkLlaveAbre','TkLlaveCierra','TkHacer'\
@@ -18,13 +18,13 @@ tokens = ('TkComa','TkPunto','TkDosPuntos','TkParAbre','TkParCierra',\
 'TkRotacion','TkTrasposicion','TkNum','TkBegin','TkId','TkTrue','TkFalse',\
 'TkCaracter','TkWhile','TkIf','TkWith','TkVar','TkEnd','TkInt','TkChar',\
 'TkBool','TkOf','TkMatrix','TkOtherwise','TkFor','TkFrom','TkTo','TkStep',\
-'TkRead','TkPrint')
+'TkRead','TkPrint','TkComenAbre','TkComenCierra','TkError')
 
 reservados = {'not':'TkNegacion','begin':'TkBegin','with':'TkWith','True':'TkTrue',\
 'False':'TkFalse','while':'TkWhile','if':'TkIf','var':'TkVar','end':'TkEnd','int':'TkInt',\
 'char':'TkChar','bool':'TkBool','of':'TkOf','matrix':'TkMatrix','otherwise':'TkOtherwise',\
 'for':'TkFor','from':'TkFrom','to':'TkTo','step':'TkStep','print':'TkPrint','read':'TkRead'}
-
+    
 
 # Regular expression rules for simple tokens
 t_TkComa = r','
@@ -58,6 +58,9 @@ t_TkRotacion = r'\$'
 t_TkTrasposicion = r'\?'
 t_TkNum = r'\d+'  ####################chequear funcion
 t_TkCaracter = r'\'(.|\\\w)\''
+t_TkComenAbre = r'\%\{'
+t_TkComenCierra = r'\}\%'
+t_ignore_linea = r'\%\%[^\n.]*'
 # A regular expression rule with some action code
 # Define a rule so we can track line numbers
 
@@ -172,23 +175,29 @@ def t_TkPrint(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-    COLUMNA[0]=1
+    COLUMNA[0]=0
+    COLUMNA[1] = 1
 # A string containing ignored characters (spaces and tabs)
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    if(COLUMNA[2]==0):
+        print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 # Build the lexer
-#t_ignore = ' \t'
+#def t_ignore_comentariolinea(t):
+#    r'\%\%[^\n.]*'
+
+
 def t_tab(t):
     r'\t+'
-    pass
-
+    #COLUMNA[0] = (COLUMNA[0] // 4)*4 + 1*(COLUMNA[0]%4==3) + 2*(COLUMNA[0]%4==2) + 3*(COLUMNA[0]%4==1) -1*(COLUMNA[0]%4!=0) 
+    COLUMNA[0] = (4-(((t.lexpos + COLUMNA[0]) )%4))*len(t.value)-(len(t.value)-1)
+    COLUMNA[1] = 0
 def t_espacio(t):
-    r'\s+'
-    pass
+    r'\s'
+    COLUMNA[1] = 0
 
 contenido = '''
 begin23 begin beginbegin with begin\n
@@ -200,18 +209,30 @@ f = open("flojera.txt",'r')
 #contenido = f.read()
 lexer = lex.lex()
 #lexer.input(contenido)
+
 while True:
+    COLUMNA[2] = 0
     for lines in f.readlines():
+        boole=True
         lexer.input(lines)
         while True:
             tok = lexer.token()
-            if not tok: 
+            if not tok:
+                if(boole==False):
+                    print('')
                 break      # No more input
-            if(tok.type=="TkId"):
-                print(str(tok.type) +"(\"" +tok.value+ "\") "+str(tok.lineno)+" "+ str(tok.lexpos)+", ",end="")
-            elif(tok.type=="TkNum"):
-                print(str(tok.type) +"(" +tok.value+ ") "+str(tok.lineno)+" "+ str(tok.lexpos)+", ",end="")
-            else:
-                print(str(tok.type) +" "+ str(tok.lineno)+" "+str(tok.lexpos+COLUMNA[0])+", ",end="")
-        print('')
+            if(tok.type=="TkComenAbre"):
+                COLUMNA[2]=1
+            elif(tok.type=="TkComenCierra"):
+                COLUMNA[2]=0
+            elif(COLUMNA[2]==0):
+                if(tok.type=="TkId"):
+                    print(str(tok.type) +"(\"" +tok.value+ "\") "+str(tok.lineno)+" "+ str(tok.lexpos+COLUMNA[0]+COLUMNA[1])+", ",end="")
+                    boole=False
+                elif(tok.type=="TkNum"):
+                    boole=False
+                    print(str(tok.type) +"(" +tok.value+ ") "+str(tok.lineno)+" "+ str(tok.lexpos+COLUMNA[0]+COLUMNA[1])+", ",end="")
+                elif tok.type != "tab" and tok.type != "espacio":
+                    boole=False
+                    print(str(tok.type) +" "+ str(tok.lineno)+" "+str(tok.lexpos+COLUMNA[0]+COLUMNA[1])+", ",end="")
     break
